90bcb93846ed246e71bb0d58e8040661a486f78c
Sahand Omarzai <sahand.omarzai@gmail.com>
2026-04-07 14:43:35 +0200

    Created a separate class to manage tasks
    implemented a method to add tasks.
    implemented a method to check the index of each task.

---
+++ ToDoListGUI/TaskManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListGUI
+{
+    class TaskManager
+    {
+        private Task[] tasks = new Task[10];
+        private int numOfTasks = 0;
+
+        public TaskManager() { }
+
+        public int GetNumTasks() 
+        { 
+            return numOfTasks; 
+        }
+
+        public Task GetTask(int index) 
+        { 
+            return tasks[index];
+        }
+
+        public void AddTask(Task task)
+        {
+            bool canAddTask = task != null && numOfTasks < tasks.Length;
+
+            if (canAddTask)
+            {
+                tasks[numOfTasks] = task;
+                numOfTasks++;
+            }
+        }
+
+        public bool CheckTask(int index)
+        {
+            bool validIndex = index >= 0 && index < numOfTasks;
+            if (validIndex)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
